8c78c198862ccca25c6891530f621b8e7ebf8616
PopielarzManon <popielarzmanon@gmail.com>
2021-05-25 17:12:03 +0200

    :clown_face: JS index

---
+++ index.js
@@ -0,0 +1,36 @@
+main()
+
+async function main() {
+    const articles = await getArticles() 
+
+    for (article of articles){
+        displayArticle(article)
+    }
+}
+
+function getArticles() {
+   return fetch ("http://localhost:3000/api/teddies")
+        .then(function(httpBodyResponse) {
+           return httpBodyResponse.json()
+        })
+        .then(function(articles){
+            return articles
+        })
+        .catch(function(error) {
+            alert(error)
+            
+        })
+    }
+function displayArticle(article){
+    document.getElementById("main").innerHTML +=
+    ` <article class="card rounded m-2 p-0 " style="width: 18rem;">
+    <img src=${article.imageUrl}>
+    <div class="card-body text-center">
+      <h5 class="card-title">${article.name}</h5>
+      <p class="card-text">${article.description}</p>
+      <a href="page2.html" class="btn btn-primary ">Voir les options</a>
+    </div>
+  </article>
+</main>
+`
+}
