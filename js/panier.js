02dd218b1636bb0d9742defa6f85f9c03dccd2a5
PopielarzManon <popielarzmanon@gmail.com>
2021-06-09 16:13:45 +0200

    :hammer: Bouton supprimer seul

--- js/panier.js
+++ js/panier.js
@@ -5,7 +5,7 @@ let itemStorage = JSON.parse(localStorage.getItem("ours"))
  const positionElementPanier = document.getElementById("recap")
  let structurePanier = [];
 
- if(itemStorage === null){
+ if(itemStorage === null || itemStorage == 0){
 const vide = `
 <div class="text-center">Votre panier est vide !</div>`
     positionElementPanier.innerHTML = vide
@@ -18,7 +18,7 @@ const vide = `
     
 for(k=0; k < itemStorage.length; k++){
         structurePanier = structurePanier + `
-        <div class="quant container d-flex justify-content-between "> ${itemStorage[k].nom} <p>${itemStorage[k].prix} $ <i class="fas fa-trash"></i></p></div>
+        <div class="quant container d-flex justify-content-between "> ${itemStorage[k].nom} <p>${itemStorage[k].prix} $ <button class="delete" ><i class="fas fa-trash"></i></button></p></div>
       </div>
         `
 }
@@ -27,3 +27,25 @@ for(k=0; k < itemStorage.length; k++){
      }
      
  }
+
+ //BTN SUP//
+
+ let deleteButton = document.querySelectorAll(".delete")
+
+//selection id//
+for (let l = 0; l < deleteButton.length; l++) {
+    deleteButton[l].addEventListener("click", (event) =>{
+    event.preventDefault();
+
+    let idDelete = itemStorage[l].idDuProduit;
+   
+    //filtre
+     itemStorage = itemStorage.filter( el => el.idDuProduit !== idDelete);
+        console.log(itemStorage);
+//local storage changement
+localStorage.setItem("ours", JSON.stringify(itemStorage));
+alert(`Vous avez suprimé un article de votre panier`);
+window.location.href = "checkout.html"
+
+    })    
+}
--- js/produit.js
+++ js/produit.js
@@ -9,33 +9,7 @@ const _id = urlSearchParams.get("id");
 fetch("http://localhost:3000/api/teddies/" + _id).then(async (result_) => {
   const response = await result_.json();
 
-  document.getElementById(
-    "produitp"
-  ).innerHTML += ` <div class="card-header border-0" id="page2">
-        <img src="${response.imageUrl}" style="width: 60vw;" alt="">
-        </div>
-        <div class="card-block px-2 pb-5 "  style="width: 25rem;">
-        <h4 class="card-title pt-4">${response.name}</h4>
-        <p class="card-text">${response.description}</p>
-        <p class="card-text"><b>${response.price / 100} $ </b></p>
-        <div class="details__options">
-        <label for="colors_select">Choisissez une couleur :</label>
-        <select class="options__select" name="colors" id="colors_select" required>
-            <option value="">--Choisissez une option--</option>
-        </select>
-      </div>
-      <button class="details__btn btn" id="addToCart" type="submit">
-        Ajouter au panier
-      </button>
- 
-        `;
-
-  for (let i = 0; i < response.colors.length; i++) {
-    const option = document.createElement("option"); //création de <option></option>
-    option.value = `${response.colors[i]}`; // ça récupère l'index du tableau <option value="brown"></option>
-    option.innerHTML = `${response.colors[i]}`; // le nom de la couleur est ajouté à <option> // <option value="brown">brown</option>
-    document.getElementById("colors_select").appendChild(option); //ça ajoute  <option value="brown">brown</option> en enfant de l'id #lense-select
-  }
+  displayArticle(response)
 
   //-----PANIER-----------//
   //ajout panier//
@@ -67,4 +41,51 @@ fetch("http://localhost:3000/api/teddies/" + _id).then(async (result_) => {
   let itemStorage = JSON.parse(localStorage.getItem("ours"));
 });
 
+function displayArticle(article) {
+  document.getElementById(
+    "produitp"
+  ).innerHTML += ` <div class="card-header border-0" id="page2">
+        <img src="${article.imageUrl}" style="width: 60vw;" alt="">
+        </div>
+        <div class="card-block px-2 pb-5 "  style="width: 25rem;">
+        <h4 class="card-title pt-4">${article.name}</h4>
+        <p class="card-text">${article.description}</p>
+        <p class="card-text"><b>${article.price / 100} $ </b></p>
+        <div class="details__options">
+        <label for="colors_select">Choisissez une couleur :</label>
+        <select class="options__select" name="colors" id="colors_select" required>
+            <option value="">--Choisissez une option--</option>
+        </select>
+      </div>
+      <button class="details__btn btn" id="addToCart" type="submit">
+        Ajouter au panier
+      </button>
+ 
+        `;
 
+  for (let i = 0; i < article.colors.length; i++) {
+    const option = document.createElement("option"); //création de <option></option>
+    option.value = `${article.colors[i]}`; // ça récupère l'index du tableau <option value="brown"></option>
+    option.innerHTML = `${article.colors[i]}`; // le nom de la couleur est ajouté à <option> // <option value="brown">brown</option>
+    document.getElementById("colors_select").appendChild(option); //ça ajoute  <option value="brown">brown</option> en enfant de l'id #lense-select
+  }
+}
+
+// cours mentorat /**
+//  * Additionne deux nombre et renvoit le résultat
+//  * @param {number} a le 1er chiffre
+//  * @param {number} b le 2eme chiffre
+//  * @returns le résultat de l'addition
+//  *
+// function addition(a, b) {
+//  return a + b;
+// }
+
+// let resultat = addition(10, 14);
+
+// let resultat2 = addition(2, 14);
+// let resultat2 = 2+14
+
+// if (resultat > 18 && resultat2 > 18) {
+//   console.log('tous les resultats sont supérieur à 18');
+// }
